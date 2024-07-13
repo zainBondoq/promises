8ddd73403c540e4e5c5f1877dee7ae891193a010
zainbondoq <zainbondoq@gmail.com>
2024-07-13 16:29:47 +0300

    second commit

--- main.js
+++ main.js
@@ -13,17 +13,44 @@
 
 
 const myPromise = new Promise(function(resolveFunction, rejectFunction) {
-    let connect = true;
+     let employees = ["osama" , "ahmad" , "sayyed" , "mahmoud"];
+ 
+     if(employees.length === 4){
+        resolveFunction(employees);
+     }else{
+        rejectFunction(Error("number of employees is not 4"))
+     }
 
-    if (connect) {
-        resolveFunction("connection established");
-    } else {
-        rejectFunction(Error("connection failed"));
-    }
+
+
+
 });
+//     let connect = true;
 
-console.log(myPromise); //sync -- will print first
+//     if (connect) {
+//         resolveFunction("connection established");
+//     } else {
+//         rejectFunction(Error("connection failed"));
+//     }
+// });
+
+// console.log(myPromise); //sync -- will print first
 myPromise.then(
-    (resolvedValue) => console.log(`good ${resolvedValue}`),
-    (rejectedValue) => console.log(`bad ${rejectedValue}`)
-)
+    (resolvedValue) => {
+        resolvedValue.length = 2;
+        return resolvedValue;
+    }
+);
+
+myPromise.then(
+    (resolvedValue) => {
+        resolvedValue.length = 1;
+        return resolvedValue;
+    }
+);
+
+myPromise.then(
+    (resolvedValue) => {
+       console.log(`the choosen employee is ${resolvedValue}`);
+    }
+).catch((rejectedReason) => console.log(rejectedReason)) .finally(console.log("operation is done"));
